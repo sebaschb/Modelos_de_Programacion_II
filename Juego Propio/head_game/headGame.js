cccbbdc87a1d5267caef62624ab8f55136242472
nyko07 <nyko07herr@gmail.com>
2019-07-04 00:54:29 -0500

    Documentación

--- Juego Propio/head_game/headGame.js
+++ Juego Propio/head_game/headGame.js
@@ -1,15 +1,15 @@
 
-
+//Definición de variables a usar
 var headGame = new Kiwi.Game();
-
 var sCampo = new Kiwi.State( "sCampo" );
 
+//En la parte del preload se cargan todas las herramientas necesarias a la hora de iniciar el juego.
 sCampo.preload = function () {
     Kiwi.State.prototype.preload.call(this);
 	
     // Adds a basic score widget to the defaultHUD of the game.
     
-    // barra score y titulo
+    // Creación de la barra de puntaje (score) y el título del juego.
     this.score = new Kiwi.HUD.Widget.BasicScore( this.game, 120, 60, 0 );
     this.scoreBest = new Kiwi.HUD.Widget.BasicScore( this.game, this.game.stage.width-50,60, 0 );
     this.title = new Kiwi.HUD.Widget.TextField( this.game, "Head 21 GAME", this.game.stage.width * 0.5-120, 20, "#000", 80, 'normal', 'Impact' );
@@ -19,7 +19,7 @@ sCampo.preload = function () {
     
     this.title.textAlign = Kiwi.GameObjects.Textfield.TEXT_ALIGN_CENTER;
     
-    
+    //Implementación del puntaje y el título en la pantalla del juego. 
     this.game.huds.defaultHUD.addWidget( this.title );
     this.game.huds.defaultHUD.addWidget( this.score );
     this.game.huds.defaultHUD.addWidget( this.scoreBest );
@@ -33,6 +33,8 @@ sCampo.preload = function () {
     //this.scoreBestText.style.background = 'white';
 
     //this.title.style.background = 'white';
+
+    //Designación y agregación de estilo al título, al puntaje y al mejor puntaje
     this.title.style.fontSize = '30px';
     this.title.style.color = ' #f79ec4 ';
     this.title.style.textShadow = '2px 2px 3px orange';
@@ -55,7 +57,8 @@ sCampo.preload = function () {
     this.score.style.color = ' #87e5f9 ';
     this.score.style.fontSize = '32px';
     this.score.style.textShadow = '2px 2px 2px orange';
-	
+    
+    //Se añaden los sprites de las imagenes a utilizar, asignando nombre, ruta y el recorte del sprite
     this.addSpriteSheet( 'jugador1', './assets/Player/jugador1.png', 100, 100 );
     this.addImage( 'background', './assets/Environment/estadio.jpg' );
     this.addSpriteSheet('piso','./assets/Environment/grass.png',131,65);
@@ -66,11 +69,11 @@ sCampo.preload = function () {
     
 }
 
-
+/*En la parte de create  */
 sCampo.create = function(){
 
     this.menuWidth = 100;
-    //menu
+    //Se crea y da estilo al menú el cual es el botón que da la opción de jugar de nuevo.
     this.volverJugarBtn = new Kiwi.HUD.Widget.MenuItem( this.game, 'Jugar otra vez', 0, 0 );
     this.volverJugarBtn.style.color = 'white';
     this.volverJugarBtn.style.display = 'block';
@@ -89,7 +92,7 @@ sCampo.create = function(){
     this.menu.getMenuItem(0).input.onDown.add( this.resetButton, this.game );
     
 
- 
+    //Se dan las especificaciones de la física que tendrá el juego.
     this.GRAVITY = 50;
     this.JUMP_SPEED = -150;
     this.jumping = false;
@@ -102,6 +105,7 @@ sCampo.create = function(){
     this.background = new Kiwi.GameObjects.StaticImage( this, this.textures.background, 0, 0 );
     this.background.transform = new Kiwi.Geom.Transform(0,0,0.4,0.3,0,0)
 
+    //Se determinan los controles de juego y como son los mivimientos del jugador
     this.jugador1 = new Kiwi.GameObjects.Sprite( this, this.textures.jugador1, this.game.stage.width/2-32, this.game.stage.height-200);
     this.leftKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.A );
     this.rightKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.D );
@@ -117,14 +121,16 @@ sCampo.create = function(){
     
     
     //this.balon.box.hitbox = new Kiwi.Geom.Rectangle(0,0,60,60);
+    //Se le agregan las fisicas al balón
     this.balon.physics = this.balon.components.add(new Kiwi.Components.ArcadePhysics(this.balon, this.balon.box));
     this.balon.physics.acceleration.y = this.GRAVITY;
     this.balon.physics.elasticity = 0.8;
     this.balon.physics.mass = 0.5;
-
+    // Como en el caso del balón, también se la agregan las fisicas al jugador
     this.jugador1.physics = this.jugador1.components.add(new Kiwi.Components.ArcadePhysics(this.jugador1, this.jugador1.box));
     this.jugador1.physics.acceleration.y = this.GRAVITY;
 
+    //Se crean unas restricciones para evitar incoherencias físicas en las colisiones
     this.facing = "right";    
     for(var x=0;x<headGame.stage.width;x+=131){
         var blockPiso = new Kiwi.GameObjects.Sprite(this, this.textures.piso,x,this.game.stage.height-65);
@@ -133,6 +139,8 @@ sCampo.create = function(){
         this.piso.addChild(blockPiso);
     }
  
+    /*Se crean los dos arcos o canchas, con sus respectivas especificaciones y postariormente se
+    agregan al escenario*/
     var arco1 = new Kiwi.GameObjects.Sprite(this, this.textures.arco1,0,this.game.stage.height-241);
     //arco1.box.hitbox = new Kiwi.Geom.Rectangle(33,0,100,0);
     //arco1.physics = arco1.components.add(new Kiwi.Components.ArcadePhysics(arco1,arco1.box));
@@ -156,6 +164,7 @@ sCampo.create = function(){
     
 }
 
+//Se reinician los valores de las variables para poder empezar un juego nuevo
 sCampo.resetButton = function () {    
     
     headGame.states.current.balon.transform.x = headGame.stage._width/2-32;
@@ -171,6 +180,9 @@ sCampo.resetButton = function () {
     
 }
 
+/*Se actualizan todas las variables del juego en el transcurso del mismo, como en el 
+caso de la colisión del jugador con el balón lo que da un punto y se tendría que 
+acualizar el puntaje, además de las posiciones de lo elementos en cuestión. */
 sCampo.update = function() {
 	
     Kiwi.State.prototype.update.call( this );
@@ -186,6 +198,8 @@ sCampo.update = function() {
 
     this.jugador1.physics.overlaps(this.balon, true);
 
+    /*Se trabajan las colisiones tanto del balon con el jugador, como del balón con el suelo, separando 
+    cada una y así poder tener una pauta de en que momento se concede un punto y en que momento se pierde el juego. */
     var jugadorColisionBalon = Kiwi.Components.ArcadePhysics.collide(this.balon,this.jugador1);
     var onTheGround = this.jugador1.physics.isTouching( Kiwi.Components.ArcadePhysics.DOWN );
     var underArco = this.jugador1.physics.isTouching(Kiwi.Components.ArcadePhysics.UP);
